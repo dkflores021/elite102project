27f9c91976f6b60c5085371c97ba498c37baf91d
Desiree  Flores <>
2021-05-13 21:42:57 +0000

    adding @client.commands such as clearing messages, kicking a member and banning a member from the discord group

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands 
 import os  #to bring in the key
 my_secret = os.environ['TOKEN']
 
@@ -12,10 +13,25 @@ class MyClient(discord.Client):
             return
 
         if message.content == 'ping':
-            await message.channel.send('pong')
+            await message.channel.send('pong!')
 
-#begin to add commands & examples from github rep. 
+#begin to add commands & examples from github rep.
+client = commands.Bot(command_prefix = '.')
 
+#clearing the past 3,5 or 10 messages
+@client.command()
+async def clear(ctx, amount=3):
+  await ctx.channel.purge(limit=amount)
+
+#kicking a member of the discord group, make the person add a reason or ask permission before kicking someone
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+  await member.kick(reason=reason)
+
+#add more deets for permissions on who can ban people
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+  await member.ban(reason=reason)
 
 
 client = MyClient()
