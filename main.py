390ea025bdb5a869593161292b099e54c34220e1
Desiree  Flores <>
2021-05-28 04:38:43 +0000

    Organizing code and editing commands

--- main.py
+++ main.py
@@ -1,40 +1,81 @@
 import discord
 from discord.ext import commands 
 import os  #to bring in the key
+
 my_secret = os.environ['TOKEN']
+client = commands.Bot(command_prefix = '?')
+client.remove_command('help')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
 
-        if message.content == 'ping':
-            await message.channel.send('pong!')
+"""Ping/Pong"""
+@client.event
+async def on_ready():
+    print('Logged on as', client.user)
+@client.event
+async def on_message(message):
+    # don't respond to ourselves
+    
+    if message.author == client.user:
+        return
 
-#begin to add commands & examples from github rep.
-client = commands.Bot(command_prefix = '.')
+    if message.content == 'ping':
+        await message.channel.send('pong!')
+    await client.process_commands(message)
 
-#clearing the past 3,5 or 10 messages
+""" Commands"""
+
+#kicks out a member
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, user:discord.Member, *, reason = None):
+    await user.kick(reason=reason)
+
+
+#bans a member
+@client.command(name="ban", help="command to ban user")
+@commands.has_permissions(ban_members=True)
+async def _ban(ctx, member: discord.Member, *, reason=None):
+    try:
+        await member.ban(reason=reason)
+        await ctx.message.delete()
+        await ctx.channel.send(f'{member.name} has been banned from server'
+                               f'Reason: {reason}')
+    except Exception:
+        await ctx.channel.send(f"Bot doesn't have enough permission to ban someone. Upgrade the Permissions")
+
+#look into why the code isn't passing the id, id isn't being calculated 
+#unbans members
+@client.command(name="unban", help="command to unban user")
+@commands.has_permissions(administrator=True)
+async def _unban(ctx, *, id):
+    """ command to unban user. check !help unban """
+    
+    await ctx.guild.unban(discord.Object(id=id))
+    await ctx.send(f"Unban {id}")
+
+#repeats the past message
+@client.command()
+async def repeat(ctx, arg):
+  await ctx.send(arg)
+
+#clears past 3 messages
 @client.command()
 async def clear(ctx, amount=3):
-  await ctx.channel.purge(limit=amount)
+  await ctx.channel.purge(limit=amount+1)        
 
-#kicking a member of the discord group, make the person add a reason or ask permission before kicking someone
-@client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
-  await member.kick(reason=reason)
+
+#begin to add commands & examples from github rep.
+
+
+# #clearing the past 3,5 or 10 messages
+
+
+
+
 
 #add more deets for permissions on who can ban people
-@client.command()
-async def ban(ctx, member : discord.Member, *, reason=None):
-  await member.ban(reason=reason)
 
-
-client = MyClient()
 client.run(my_secret)
 my_secret = os.environ['TOKEN']
 print (my_secret)
